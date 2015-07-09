155540103de910c5eeedf0edc40f759aec371d1d
FlashCat <FlashCat@users.noreply.github.com>
2015-07-09 22:38:51 +0000

    rebuild pages at 186e621

--- implementors/core/iter/trait.IntoIterator.js
+++ implementors/core/iter/trait.IntoIterator.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['bit_vec'] = ["impl&lt;'a, B: <a class='trait' href='bit_vec/trait.BitBlock.html' title='bit_vec::BitBlock'>BitBlock</a>&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.IntoIterator.html' title='core::iter::IntoIterator'>IntoIterator</a> for &amp;'a <a class='struct' href='bit_vec/struct.BitVec.html' title='bit_vec::BitVec'>BitVec</a>&lt;B&gt;",];
+implementors['bit_vec'] = ["impl&lt;'a, B: <a class='trait' href='bit_vec/trait.BitBlock.html' title='bit_vec::BitBlock'>BitBlock</a>&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.IntoIterator.html' title='core::iter::IntoIterator'>IntoIterator</a> for &amp;'a <a class='struct' href='bit_vec/struct.BitVec.html' title='bit_vec::BitVec'>BitVec</a>&lt;B&gt;","impl&lt;B: <a class='trait' href='bit_vec/trait.BitBlock.html' title='bit_vec::BitBlock'>BitBlock</a>&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.IntoIterator.html' title='core::iter::IntoIterator'>IntoIterator</a> for <a class='struct' href='bit_vec/struct.BitVec.html' title='bit_vec::BitVec'>BitVec</a>&lt;B&gt;",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
